22ce1d1eb4df91e61fa8975505f2a051a131bc04
Tarun Gupta <55084576+tgboyy@users.noreply.github.com>
2020-04-03 13:52:25 +0530

    Create CODE 3.C

---
+++ CODE 3.C
@@ -0,0 +1,15 @@
+//PROGRAM TO COMPARE TWO NUMBERS TO FING GREATER NUMBER.
+#include <stdio.h>
+int main()
+{
+	int n,m;
+	printf(" Enter a Number  :  ");
+	scanf("%d",&n);
+	printf(" Enter a Number  :  ");
+	scanf("%d",&m);
+	if(n>m)
+   	printf("Greater number is  :  %d",n);
+   	else
+     	printf("Greater number is  :  %d",m);
+	return 0;
+}
