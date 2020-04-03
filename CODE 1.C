c3524a15087d8f274f36e4b07ae95b42213acc0e
Tarun Gupta <55084576+tgboyy@users.noreply.github.com>
2020-04-03 13:38:39 +0530

    Create CODE 1.C

---
+++ CODE 1.C
@@ -0,0 +1,17 @@
+/* get number input from the user and print if it is odd print weird */
+#include<stdio.h>
+void main()
+{ 
+   int(i);
+   printf("give any no.");
+   scanf("%d",&i);
+   if(i/2==0)
+   {
+       printf("the no is even %d ",i);
+   }
+   else
+   {
+       printf("weird");
+   }
+ 
+}
