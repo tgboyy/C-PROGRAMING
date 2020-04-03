fa17ae177fbca24634ff6e0f7a70717d0a87b1b2
Tarun Gupta <55084576+tgboyy@users.noreply.github.com>
2020-04-03 14:02:12 +0530

    Create CODE 10.C

---
+++ CODE 10.C
@@ -0,0 +1,13 @@
+// PROGRAM TO CHECK WHETHER A CHARACTER IS A VOWEL OR A CONSONENT.
+#include <stdio.h>
+int main()
+{
+	char n;
+	printf(" Enter an alphabet  :  ");
+	scanf("%c",&n);
+    if((n=='a')||(n=='e')||(n=='i')||(n=='o')||(n=='u')||(n=='A')||(n=='E')||(n=='I')||(n=='O')||(n=='U'))
+   	printf(" Alphabet is a vowel ");
+     	else
+       	printf(" Alphabet is a consonent ");
+	return 0;
+}
