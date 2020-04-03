7bfc8835b481898df2f67010ed0259aec2d859d0
Tarun Gupta <55084576+tgboyy@users.noreply.github.com>
2020-04-03 13:57:07 +0530

    Create CODE 7.C

---
+++ CODE 7.C
@@ -0,0 +1,13 @@
+// PROGRAM TO CHECK WHETHER A NUMBER IS ODD OR EVEN NUMBER.
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf(" Enter a Number  :  ");
+	scanf("%d",&n);
+	if(n%2==0)
+   	printf(" Even Number");
+     	else
+        	printf(" Odd Number");
+	return 0;
+}
