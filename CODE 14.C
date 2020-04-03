b0ba4ce857f97b62d0460a5f3004dd0aeb792d64
Tarun Gupta <55084576+tgboyy@users.noreply.github.com>
2020-04-03 14:16:51 +0530

    Create CODE 14.C

---
+++ CODE 14.C
@@ -0,0 +1,38 @@
+// PROGRAM TO FIND NO. OF DAYS IN A MONTH.
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+	int n;
+	printf(" Enter month number (1-12)  :  ");
+	scanf("%d",&n);
+ 	switch(n)
+ 	{
+    	case 1:  printf("number of days = 31");
+             	break;
+    	case 2:  printf("number of days = 28/29");
+             	break;
+    	case 3:  printf("number of days = 31");
+             	break;
+    	case 4:  printf("number of days = 30");
+             	break;
+    	case 5:  printf("number of days = 31");
+             	break;
+    	case 6:  printf("number of days = 30");
+             	break;
+    	case 7:  printf("number of days = 31");
+             	break;
+    	case 8:  printf("number of days = 31");
+             	break;
+    	case 9:  printf("number of days = 30");
+             	break;
+    	case 10: printf("number of days = 31");
+             	break;
+    	case 11: printf("number of days = 30");
+             	break;
+    	case 12: printf("number of days = 31");
+             	break;
+    	default: printf("!!**Invalid input**!!");
+ 	}
+	return 0;
+}
