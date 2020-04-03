afc4eb84c7920cfdb70d005b7ff98003075fb6b1
Tarun Gupta <55084576+tgboyy@users.noreply.github.com>
2020-04-03 13:54:29 +0530

    Create CODE 5.C

---
+++ CODE 5.C
@@ -0,0 +1,17 @@
+//PROGRAM TO CHECK WETHER A NUMBER IS POSITIVE OR NEGATIVE NUMBER.
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf(" Enter a Number  :  ");
+	scanf("%d",&n);
+	if(n>0)
+   	printf("Positive number ");
+     	else
+       	if(n<0)
+        	printf("Negative number ");
+          	else
+            	if(n==0)
+             	printf("Number is Zero ");
+	return 0;
+}
