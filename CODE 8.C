82d54af8d3a7152167a7485e2b1066c32addc1e7
Tarun Gupta <55084576+tgboyy@users.noreply.github.com>
2020-04-03 13:58:04 +0530

    Create CODE 8.C

---
+++ CODE 8.C
@@ -0,0 +1,13 @@
+// PROGRAM TO CHECK WHETHER A YEAR IS LEAP YEAR OR NOT.
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf(" Enter year  :  ");
+	scanf("%d",&n);
+	if(n%4==0)
+   	printf(" Leap year");
+     	else
+        	printf(" Not Leap year");
+	return 0;
+}
