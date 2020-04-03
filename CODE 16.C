381a45bf6159eaffe60c77563691ca8721b24585
Tarun Gupta <55084576+tgboyy@users.noreply.github.com>
2020-04-03 14:23:17 +0530

    Create CODE 16.C

---
+++ CODE 16.C
@@ -0,0 +1,18 @@
+// PROGRAM TO CHECK WHETHER A TRIANGLE IS POSSIBLE BY CHECKING ITS ANGLES.
+#include <stdio.h>
+int main()
+{
+	int n,a1,a2,a3;
+	printf(" Enter angle 1  :  ");
+	scanf("%d",&a1);
+	printf(" Enter angle 2  :  ");
+	scanf("%d",&a2);
+	printf(" Enter angle 3  :  ");
+	scanf("%d",&a3);
+	n=a1+a2+a3;
+	if(n==180)
+   	printf(" Triangle is possible ");
+     	else
+        	printf(" Triangle is not possible ");
+	return 0;
+}
