1892ebb5cbf5bb4b7dd035b336d471a7f9aaa944
Tarun Gupta <55084576+tgboyy@users.noreply.github.com>
2020-04-10 00:04:07 +0530

    Create CODE 34.C

---
+++ CODE 34.C
@@ -0,0 +1,17 @@
+/* PROGRAM TO REVERSE A NUMBER. */
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,d,s=0;
+printf("Enter the number");
+scanf("%d",&n);
+while(n>0)
+{
+d=n%10;
+s=s*10+d;
+n=n/10;
+}
+printf("The reverse is %d",s);
+getch();
+}
