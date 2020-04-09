67ad595d5ed4e20c471f31bfa667c3a6f6404b39
Tarun Gupta <55084576+tgboyy@users.noreply.github.com>
2020-04-10 00:20:02 +0530

    Create CODE 38.C

---
+++ CODE 38.C
@@ -0,0 +1,17 @@
+/* PROGRAM TO FIND FIBONACCI SERIES. */
+#include<stdio.h>
+#include<conio.h>
+int main()
+{ 
+ int i,n,c,a=0,b=1;
+ printf("enter the number of terms  :  "); 
+	scanf("%d",&n);
+ for (i=0;i<n;++i) 
+	{
+        c=a+b;
+        a=b;
+        b=c;
+		printf("\n%d",c);
+    }
+   getch();
+}
